43a5b668a9cd981984faf4be78a1099e3c0acde0
shammasnhd@gmail.com <Shammaz77>
2023-10-17 12:01:46 +0530

    a

--- js/main.js
+++ js/main.js
@@ -19,59 +19,59 @@ window.addEventListener('load', checkFade); // For the initial check
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    let upto = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//     let upto = 0;
 
-    function updated() {
-        let count = document.getElementById("counter");
-        count.innerHTML = ++upto + "+";
-        if (upto === 5) {
-            clearInterval(counts);
-        }
-    }
+//     function updated() {
+//         let count = document.getElementById("counter");
+//         count.innerHTML = ++upto + "+";
+//         if (upto === 5) {
+//             clearInterval(counts);
+//         }
+//     }
 
-    // Set the interval with a delay of 40 milliseconds
+//     // Set the interval with a delay of 40 milliseconds
 
 
-});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-    let upto1 = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//     let upto1 = 0;
 
-    function updated1() {
-        let count = document.getElementById("counter1");
-        count.innerHTML = ++upto1 + "+";
-        if (upto1 === 5) {
-            clearInterval(counts1);
-        }
-    }
+//     function updated1() {
+//         let count = document.getElementById("counter1");
+//         count.innerHTML = ++upto1 + "+";
+//         if (upto1 === 5) {
+//             clearInterval(counts1);
+//         }
+//     }
 
-    let counts1 = setInterval(updated1, 40);
+//     let counts1 = setInterval(updated1, 40);
 
-    let upto2 = 0;
+//     let upto2 = 0;
 
-    function updated2() {
-        let count = document.getElementById("counter2");
-        count.innerHTML = ++upto2 + "+";
-        if (upto2 === 100) {
-            clearInterval(counts2);
-        }
-    }
+//     function updated2() {
+//         let count = document.getElementById("counter2");
+//         count.innerHTML = ++upto2 + "+";
+//         if (upto2 === 100) {
+//             clearInterval(counts2);
+//         }
+//     }
 
-    let counts2 = setInterval(updated2, 40);
+//     let counts2 = setInterval(updated2, 40);
 
-    let upto3 = 0;
+//     let upto3 = 0;
 
-    function updated3() {
-        let count = document.getElementById("counter3");
-        count.innerHTML = ++upto3 + "+";
-        if (upto3 === 2) {
-            clearInterval(counts3);
-        }
-    }
+//     function updated3() {
+//         let count = document.getElementById("counter3");
+//         count.innerHTML = ++upto3 + "+";
+//         if (upto3 === 2) {
+//             clearInterval(counts3);
+//         }
+//     }
 
-    let counts3 = setInterval(updated3, 40);
-});
+//     let counts3 = setInterval(updated3, 40);
+// });
 
 
 
