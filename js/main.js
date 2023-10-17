0ffe90881ffb365cb2471854d3db1be62130faa3
shammasnhd@gmail.com <Shammaz77>
2023-10-17 11:31:32 +0530

    almsot added!!

--- js/main.js
+++ js/main.js
@@ -79,6 +79,33 @@ window.addEventListener('load', checkFade); // For the initial check
 
 
 
-
+function toggleCartt(){
+    document.querySelector('.sidebar').classList.toggle('open-cart');
+    }
+              // Get references to the file input and button
+                      const fileInput = document.getElementById('fileInput');
+          const uploadButton = document.getElementById('uploadButton');
+          const fileNameDisplay = document.getElementById('fileName');
+  
+          // Add a click event listener to the button
+          uploadButton.addEventListener('click', () => {
+              // Programmatically trigger a click event on the hidden file input
+              fileInput.click();
+          });
+  
+          // Add an event listener to the file input to handle file selection
+          fileInput.addEventListener('change', () => {
+              // Get the selected file (if any)
+              const selectedFile = fileInput.files[0];
+  
+              // Display the selected file name
+              if (selectedFile) {
+                  fileNameDisplay.textContent = `Selected File: ${selectedFile.name}`;
+              } else {
+                  fileNameDisplay.textContent = 'No file selected';
+              }
+  
+              // You can now send the selected file to your server using AJAX or other methods for further processing.
+          });
 
 
